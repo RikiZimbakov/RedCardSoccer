2391aa4faf71a170f82a77188cb8c90bc0292909
Yutuc <psyutuc@hotmail.com>
2019-08-23 16:16:22 -0500

    - Notifications implemented

--- app/src/main/java/com/example/redcardsoccer/MainActivity.kt
+++ app/src/main/java/com/example/redcardsoccer/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.redcardsoccer
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -15,13 +23,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var active = false
+    }
+
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+
+    private val channelId = "com.example.redcardsoccer"
+    private val description = "Red Card Soccer"
+    private val notificationID = 1999
+
     val promotionsArrayList = ArrayList<PromotionObject>()
     val adapter = GroupAdapter<ViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //setTitle("Main")
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        setTitle("RedCardSoccer Retail Store")
         recyclerview_main.adapter = adapter
         pullPromotions()
     }
@@ -57,6 +80,7 @@ class MainActivity : AppCompatActivity() {
                 val promotionObject = p0.getValue(PromotionObject::class.java)!!
                 promotionsArrayList.add(promotionObject)
                 refreshRecyclerView()
+                showNotification(promotionObject.description)
             }
 
             override fun onChildRemoved(p0: DataSnapshot) {
@@ -70,5 +94,53 @@ class MainActivity : AppCompatActivity() {
         promotionsArrayList.forEach{
             adapter.add(PromotionRow(it))
         }
+    }
+
+    private fun showNotification(pulledDescription: String){
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this,0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        if(!active) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this, channelId)
+                    .setContentTitle("New post!")
+                    .setContentText(pulledDescription)
+                    .setAutoCancel(true)
+                    .setPriority(Notification.PRIORITY_DEFAULT)
+                    .setStyle(Notification.BigTextStyle().bigText(pulledDescription))
+                    .setSmallIcon(R.drawable.abc_ic_star_black_48dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.abc_ic_star_black_48dp))
+                    .setContentIntent(pendingIntent)
+            }
+            else {
+                builder = Notification.Builder(this)
+                    .setContentTitle("New post!")
+                    .setContentText(pulledDescription)
+                    .setAutoCancel(true)
+                    .setPriority(Notification.PRIORITY_DEFAULT)
+                    .setStyle(Notification.BigTextStyle().bigText(pulledDescription))
+                    .setSmallIcon(R.drawable.abc_ic_star_black_48dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.abc_ic_star_black_48dp))
+                    .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(notificationID, builder.build())
+        }
+    }
+
+    override fun onStart() {
+        active = true
+        super.onStart()
+    }
+
+    override fun onStop() {
+        active = false
+        super.onStop()
     }
 }
